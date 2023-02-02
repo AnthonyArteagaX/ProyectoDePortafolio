85f028ba62183e3425787c98420f2fd07ebdcfdf
Anthony Rafael Arteaga Guaman <arteaga.anthonyp2021@gmail.com>
2023-02-01 23:00:18 -0500

    incluye configuracion de webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './assets/js/fixed.js';
+import './assets/js/main.js';
+//import '../node_modules/eins-modal/dist/js/eins-modal.js'
+import '/Users/maril/OneDrive/Escritorio/VALEYA/node_modules/eins-modal';
+
+import '../CSS/index.css';
